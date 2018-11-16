44cf66776dca2d2085dcc41ed02756330c788d86
Jack T <jtabb1256@gmail.com>
2018-11-16 00:50:01 -0500

    Having trouble importing composer and zxcvbn in the php code

--- models/Owner.php
+++ models/Owner.php
@@ -6,7 +6,7 @@
     // Import the interface that each model implements
     include_once 'model_interface.php';
     // Import Composer
-    require_once '../bin/vendor/autoload.php';
+    // require_once '../bin/vendor/autoload.php';
     // Use the library for checking strength of passwords
     use ZxcvbnPhp\Zxcvbn;
 

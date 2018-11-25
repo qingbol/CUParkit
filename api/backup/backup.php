0961b056ac992f03f7bb62a270ac6221804dde90
qingbol <qingbol@g.clemson.edu>
2018-11-24 22:51:36 -0500

    Implement backup funciton.

---
+++ api/backup/backup.php
@@ -0,0 +1,28 @@
+<?php
+date_default_timezone_set('America/New_York'); 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$database = 'cpsc6620';
+$user = 'root';
+$pass = '0000';
+$host = 'localhost';
+
+$date_string = date("Ymd");
+$dir = dirname(__FILE__) . '/' . $database . '_' . $date_string . '.sql';
+
+echo "<h3>Backing up database to `<code>{$dir}</code>`</h3>";
+
+exec("mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$dir} 2>&1", $output);
+
+var_dump($output);
+
+// var_dump(get_current_user());
+// $stmt = 'php -v';
+// exec($stmt, $arr);
+// echo '<pre>';
+// var_dump($arr);
+// echo "I am ~~~ \n";
+// echo shell_exec('ls') ;
+// echo shell_exec('whoami');

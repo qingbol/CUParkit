7120f3e26e7fff2c6a08b07423304c735d0b9444
qingbol <qingbol@g.clemson.edu>
2018-11-28 00:33:41 -0500

    Using bootstrap to modify html file.

--- config/Database.php
+++ config/Database.php
@@ -1,6 +1,5 @@
 <?php
     class Database {
-<<<<<<< HEAD
         // Production Database Parameters
         // private $host = "mysql1.cs.clemson.edu";
         // private $db_name = "CPSC_4620_c0ei";
@@ -11,33 +10,39 @@
        
         // // Jack localhost DB Params
         // private $host = "192.168.122.173";
-        private $host = "localhost";
-        private $db_name = "CUParkit";
-        private $charset = "utf8";
-        private $username = "parkingMgr";
-        private $password = "abc123";
-        private $conn;
+        // private $host = "localhost";
+        // private $db_name = "CUParkit";
+        // private $charset = "utf8";
+        // private $username = "parkingMgr";
+        // private $password = "abc123";
+        // private $conn;
 
         // // localhost Database Parameters
         // private $host = "localhost";
         // private $db_name = "cpsc6620";
-=======
         // Database Parameters
         // private $host = "mysql1.cs.clemson.edu";
         // private $db_name = "CPSC_4620_c0ei";
->>>>>>> 686e8589302105dc7b80d7103d01cf7e70aa9611
         // private $charset = "utf8";
         // private $username = "CPSC_4620_a31t";
         // private $password = "SoQoolLearning1";
         // private $conn;
-       
+
+       // Qingbo remote Database Parameters
+       private $host = "mysql1.cs.clemson.edu";
+       private $db_name = "parkit";
+       private $charset = "utf8";
+       private $username = "qbl";
+       private $password = "lqb987741";
+       private $conn;
+
         // localhost Database Parameters
-        private $host = "localhost";
-        private $db_name = "cpsc6620";
-        private $charset = "utf8";
-        private $username = "root";
-        private $password = "0000";
-        private $conn; // Connection to the database
+        // private $host = "localhost";
+        // private $db_name = "cpsc6620";
+        // private $charset = "utf8";
+        // private $username = "root";
+        // private $password = "0000";
+        // private $conn; // Connection to the database
         
         // Connect to Database
         public function connect() {

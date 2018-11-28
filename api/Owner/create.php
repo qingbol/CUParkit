70ba992d281c89d4bc3ba550a66e6ead88af2bcc
Jack T <jtabb1256@gmail.com>
2018-11-28 00:21:57 -0500

    TODO: Need to add try / catch for server errors to all api endpoints. Either using the commented-out way or this new way

--- api/Owner/create.php
+++ api/Owner/create.php
@@ -9,11 +9,13 @@
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-    // Include the database
-    include_once '../../config/Database.php';
+    /* OLD METHOD */
+    ////////////////////////////////////////////////////////////////////////////
+    // // Include the database
+    // include_once '../../config/Database.php';
 
-    // Include the data model that communicates to the database
-    include_once '../../models/Owner.php';
+    // // Include the data model that communicates to the database
+    // include_once '../../models/Owner.php';
 
     // Using try / catch to handle any server errors (typically 500)
     // try {
@@ -44,9 +46,33 @@
     //     echo json_encode( array("message" => $e->getMessage(),
     //                             "code" => (int)$e->getCode()) );
     // }
+    ////////////////////////////////////////////////////////////////////////////
 
+
+
+    /* NEW METHOD */
+    ////////////////////////////////////////////////////////////////////////////
+    // Include the database
+    include_once '../../config/Database.php';
+    // Include the data model that communicates to the database
+    include_once '../../models/Owner.php';
+    // Include the trait that all api endpoints share
     include_once '../catch_err_trait.php';
 
+    /* NOTE:
+     * Using a class so I can use a trait.
+     * Using a trait to reduce repeated try / catch statement in all api endpoints.
+     * The try / catch statement is necessary for server errors that would otherwise 
+     *   only give a 500ish code back.
+     *   With the statement, we get more details sent back to the client.
+     * Feeding the function imported from the trait with an anonymous function
+     * 
+     * The end result of this convolution is to perform the code that is contained 
+     *   within the anonymous function, but within a try / catch statement.
+     * 
+     * Also, this allows the use of a trait for reducing the redundant code of 
+     *   connecting to the database and instantiating the corresponding model object.
+     */
     class Create {
         use catch_err_trait;
 
@@ -80,4 +106,6 @@
         }
     }
     
+    // call the class's constructor, which will run the code for this api endpoint.
     $create = new Create();
+    ////////////////////////////////////////////////////////////////////////////
